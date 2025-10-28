ba096521393d2f8cb5fe733a7d8b5d3f6d33aa43
PAULdGuez <paulhernandezdominguez10@gmail.com>
2025-10-28 00:30:15 -0600

    ANTES DE AÑADIR ELEMENTOS COMPLEJOS

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_cors import CORS # Importa CORS
+
+# 1. Crea la aplicación del servidor
+app = Flask(__name__)
+# 2. Permite que tu página web (de otro origen) le hable a este servidor
+CORS(app) 
+
+# 3. Define una "ruta" o URL que podemos llamar
+# Esto crea la dirección: http://127.0.0.1:5000/clic-boton
+@app.route("/clic-boton")
+def manejar_clic():
+    # 4. Esta es la acción que querías:
+    # Se imprimirá en la terminal donde ejecutes este script
+    print("¡El botón del portafolio fue presionado!")
+    
+    # 5. Devuelve una respuesta al navegador
+    return {"mensaje": "¡Recibido en el servidor!"}
+
+@app.route("/clic-btres")
+def manejar_ctres():
+    print("el boton tres a sido presionado")
+    return{"mensaje" : "recibido en el servidor"}
+# 6. Ejecuta el servidor cuando corras el archivo
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
---
+++ js/index.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const miBoton = document.getElementById('btn-portafolio');
+    const bototres = document.getElementById('botontres');
+
+    // "async" le dice a la función que espere una respuesta
+    miBoton.addEventListener('click', async function(evento) {
+        
+        // Evita que el enlace '#' recargue la página
+        evento.preventDefault(); 
+
+        try {
+            // 1. Llama al "mesero" (fetch) y dale la URL de tu servidor
+            const respuesta = await fetch('http://127.0.0.1:5000/clic-boton');
+            
+            // 2. Espera la respuesta del servidor
+            const data = await respuesta.json();
+            
+            // 3. Muestra la respuesta del servidor en la consola del NAVEGADOR
+            console.log('Respuesta del servidor:', data.mensaje);
+
+        } catch (error) {
+            // 4. Si el servidor está apagado o hay un error
+            console.error('Error al conectar con el servidor:', error);
+            alert('Error: No se pudo conectar al servidor Python.');
+        }
+    
+    });
+
+    if (bototres){
+        bototres.addEventListener('click', async function(evento){
+            evento.preventDefault()
+            try{
+                const respuesta = await fetch('http://127.0.0.1:5000/clic-btres')
+                const data = await respuesta.json();
+                console.log('RESPUESTA DEL SERVIDOR: ', data.mensaje)
+            } catch(error){
+                console.warn('Error con el servidor: ', error);
+                alert('ERROR AL CONECAT CON EL SERVIDOR');
+            }
+        });
+    }
+
+   
+
+
+});
